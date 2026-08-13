b2f6b5d4fec4621765038b023cd2dd0dea4e9b4c
Nooneboi <134939435+Nooneboi@users.noreply.github.com>
2026-08-13 18:26:06 +0630

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
   browsers pick up the update instead of serving a stale cache.
 */
 
-const CACHE_NAME = "studo-v15-radio";
+const CACHE_NAME = "studo-v16-radio-clean";
 const CORE_ASSETS = [
   "index.html",
   "404.html",
